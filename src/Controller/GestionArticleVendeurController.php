bf28922f314e1dab96ea1f024b31b85f1ffa0834
Tobokoue benilde <benilde@codings-MacBook-Pro-2.local>
2024-10-03 17:05:04 +0200

    ajout de fonctionaliter

--- src/Controller/GestionArticleVendeurController.php
+++ src/Controller/GestionArticleVendeurController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 class GestionArticleVendeurController extends AbstractController
 {
 
-    #[Route(name:'app_gestion_aticle')]
+    #[Route(name:'app_gestion_article')]
     public function listArticles(EntityManagerInterface $entityManager): Response
     {
         // Récupérer le repository de l'entité Article
@@ -68,5 +68,34 @@ class GestionArticleVendeurController extends AbstractController
         return $this->render('gestion_article_vendeur/affiche.html.twig', [
             'article' => $article,
         ]);
+    }
+    
+    #[Route('/{id}/modification', name: 'app_article_modification', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(GestionArticleVendeurType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_gestion_article', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('gestion_article_vendeur/modification.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_article_suppression', methods: ['POST'])]
+    public function delete(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_gestion_article', [], Response::HTTP_SEE_OTHER);
     }
 }
